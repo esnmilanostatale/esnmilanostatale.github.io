3dba941496e4eb08ef6fc1512493edebc0087bcc
meliot <meliot@protonmail.com>
2019-01-26 18:14:57 +0100

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,5 +5,21 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
-alert(1)
+});
+ /*Scroll to top when arrow up clicked END*/
