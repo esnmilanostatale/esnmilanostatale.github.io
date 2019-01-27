b7e1a8121c7efe7fb51a2df33525aa3d789823cb
meliot <meliot@protonmail.com>
2019-01-28 00:16:23 +0100

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 });
  /*Scroll to top when arrow up clicked END*/
 $(window).bind('scroll', function() {
-         if ($(window).scrollTop() > 230) {
+         if ($(window).scrollTop() > 220) {
              $('#openMenu').css("filter", "brightness(1)");
          }
          else {
