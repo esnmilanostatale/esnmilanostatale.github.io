1e0a1dfd5dcc0f294dd1e9e1702daaad8f945c2b
meliot <meliot@protonmail.com>
2019-01-28 00:03:36 +0100

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,11 @@ $(document).ready(function() {
 
 });
  /*Scroll to top when arrow up clicked END*/
+$(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 50) {
+             $('#openMenu').css("filter", "brightness(100)");
+         }
+         else {
+             $('#openMenu').css("filter", "brightness(50)");
+         }
+    });
